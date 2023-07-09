96f643685d2276de0e80990a77164d7785e444bd
wzhqwq <wzhii345@outlook.com>
2023-07-10 00:16:27 +0800

    实现参数的按需渲染，以及修改标记

--- src/component/basic/ParameterInput.tsx
+++ src/component/basic/ParameterInput.tsx
@@ -1,6 +1,6 @@
 import { EachTypeOfConfigParameter } from '@/types/config/parameter'
 import { FormLabel, Chip, FormHelperText, Switch, Input, Select, Box, Option } from '@mui/joy'
-import { Control, Controller } from 'react-hook-form'
+import { Control, Controller, ControllerRenderProps, Field, useWatch } from 'react-hook-form'
 import { Tuple2Input } from './CustomInput'
 import FormModal from './FormModal'
 
@@ -11,68 +11,73 @@ export interface ParameterInputProps {
 }
 
 export default function ParameterInput({ prefix, parameter, control }: ParameterInputProps) {
+  const renderInput = (field: ControllerRenderProps) =>
+    parameter.type == 'bool' ? (
+      <Switch {...field} onChange={field.onChange} checked={field.value} sx={{ my: 0.5 }} />
+    ) : parameter.type == 'tuple2' ? (
+      <Tuple2Input {...field} sx={{ my: 0.5 }} size="sm" />
+    ) : parameter.properties ? (
+      <FormModal
+        control={control}
+        name={(prefix ? prefix + '.' : '') + parameter.key}
+        parameters={parameter.properties}
+        label={parameter.key as string}
+        description={parameter.description}
+      />
+    ) : parameter.selections ? (
+      <Select
+        {...field}
+        onChange={(_, value) =>
+          field.onChange(parameter.type == 'int' ? parseInt(value) : parameter.selections![value])
+        }
+        size="sm"
+      >
+        {parameter.selections?.map((selection, index) => (
+          <Option value={index} key={selection}>
+            {selection}
+          </Option>
+        ))}
+      </Select>
+    ) : parameter.type == 'int' || parameter.type == 'float' ? (
+      <Input
+        onChange={(e) => field.onChange(Number(e.target.value))}
+        value={field.value}
+        ref={field.ref}
+        sx={{ my: 0.5 }}
+        size="sm"
+      />
+    ) : (
+      <Input {...field} sx={{ my: 0.5 }} size="sm" />
+    )
+  const form = useWatch({
+    control,
+    name: prefix ?? '',
+    disabled: !parameter.shown,
+  })
+  const show = parameter.shown ? parameter.shown(form) : true
+  
   return (
-    <Box>
-      <FormLabel>
-        {parameter.key as string}
-        <Chip size="sm" variant="soft" sx={{ ml: 1 }}>
-          {parameter.type}
-        </Chip>
-      </FormLabel>
-      <FormHelperText>{parameter.description}</FormHelperText>
-      {parameter.type == 'dict' && parameter.properties ? (
-        <FormModal
-          control={control}
-          name={(prefix ? prefix + '.' : '') + parameter.key}
-          parameters={parameter.properties}
-          label={parameter.key as string}
-          description={parameter.description}
-        />
-      ) : (
-        <Controller
-          control={control}
-          name={(prefix ? prefix + '.' : '') + parameter.key}
-          defaultValue={parameter.default}
-          render={
-            parameter.type == 'bool'
-              ? ({ field: { onChange, value, ref } }) => (
-                  <Switch onChange={onChange} checked={value} ref={ref} sx={{ my: 0.5 }} />
-                )
-              : parameter.type == 'tuple2'
-              ? ({ field }) => <Tuple2Input {...field} sx={{ my: 0.5 }} size="sm" />
-              : parameter.selections
-              ? ({ field: { onChange, value, ref } }) => (
-                  <Select
-                    onChange={(_, value) =>
-                      onChange(
-                        parameter.type == 'int' ? parseInt(value) : parameter.selections![value],
-                      )
-                    }
-                    value={value}
-                    ref={ref}
-                    size="sm"
-                  >
-                    {parameter.selections?.map((selection, index) => (
-                      <Option value={index} key={selection}>
-                        {selection}
-                      </Option>
-                    ))}
-                  </Select>
-                )
-              : parameter.type == 'int' || parameter.type == 'float'
-              ? ({ field: { onChange, value, ref } }) => (
-                  <Input
-                    onChange={(e) => onChange(Number(e.target.value))}
-                    value={value}
-                    ref={ref}
-                    sx={{ my: 0.5 }}
-                    size="sm"
-                  />
-                )
-              : ({ field }) => <Input {...field} sx={{ my: 0.5 }} size="sm" />
-          }
-        />
+    <Controller
+      control={control}
+      name={(prefix ? prefix + '.' : '') + parameter.key}
+      defaultValue={parameter.default}
+      render={({ field, fieldState }) => (
+        <Box display={show ? 'block' : 'none'}>
+          <FormLabel>
+            {parameter.key as string}
+            <Chip size="sm" variant="soft" sx={{ ml: 1 }}>
+              {parameter.type}
+            </Chip>
+            {fieldState.isDirty && (
+              <Chip size="sm" variant="solid" sx={{ ml: 1 }}>
+                已修改
+              </Chip>
+            )}
+          </FormLabel>
+          <FormHelperText>{parameter.description}</FormHelperText>
+          {renderInput(field)}
+        </Box>
       )}
-    </Box>
+    />
   )
 }
