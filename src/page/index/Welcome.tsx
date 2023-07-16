df3e72e1d8ab57fb147415dd4be7d3022f480407
wzhqwq <wzhii345@outlook.com>
2023-07-16 11:38:14 +0800

    首页调整

--- src/page/index/Welcome.tsx
+++ src/page/index/Welcome.tsx
@@ -9,22 +9,23 @@ export default function Welcome() {
   const navigate = useNavigate()
   return (
     <Stack alignItems="center" justifyContent="center" height="100%">
-      <Stack alignItems="center">
+      <Stack alignItems="center" mt={4}>
         <img src={logo} alt="logo" width="200" />
         <Typography level="h4">多聚类可视化平台</Typography>
         <Stack direction="row" alignItems="center" spacing={0.5} mt={4}>
-          <StepBlock step={1} title="上传数据集" badgeContent="xxx个在线公开数据集">
+          <StepBlock step={1} title="上传数据集" badgeContent="3个在线公开数据集">
             <ul>
               <li>生物数据</li>
               <li>文本数据</li>
               <li>图像数据</li>
+              <li>中间数据</li>
             </ul>
             <Button sx={{ mt: 1 }} fullWidth onClick={() => navigate('/explore/datasets')}>
               探索数据集
               <ChevronRightRounded />
             </Button>
           </StepBlock>
-          <StepBlock step={2} title="预处理数据" badgeContent="xxx个预处理算法">
+          <StepBlock step={2} title="预处理数据" badgeContent="4种方向的预处理">
             <ul>
               <li>序列编码压缩</li>
               <li>标点删除</li>
@@ -37,21 +38,21 @@ export default function Welcome() {
               <ChevronRightRounded />
             </Button>
           </StepBlock>
-          <StepBlock step={3} title="运行模型" badgeContent="xxx个可配置模型">
+          <StepBlock step={3} title="运行模型" badgeContent="无数种可配置模型">
             <ul>
               <li>多双聚类</li>
               <li>深度多聚类</li>
               <li>原始空间聚类</li>
               <li>子空间多聚类</li>
               <li>深度学习</li>
-              <li>用户自上传模型</li>
+              {/* <li>用户自上传模型</li> */}
             </ul>
             <Button sx={{ mt: 1 }} fullWidth onClick={() => navigate('/explore/models')}>
               探索模型
               <ChevronRightRounded />
             </Button>
           </StepBlock>
-          <StepBlock step={4} title="可视化分析" badgeContent="xxx个可视化分析模块">
+          <StepBlock step={4} title="可视化分析" badgeContent="3个可视化分析模块">
             <ul>
               <li>聚类结果可视化</li>
               <li>质量评估</li>
